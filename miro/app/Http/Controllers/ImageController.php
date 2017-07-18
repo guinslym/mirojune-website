54bdb1cbc453a7fcc1aa969609c0c6bbc0e57456
Guinslym <guinslym@gmail.com>
2017-07-17 20:39:09 -0400

    new file and add html service provider

--- miro/app/Http/Controllers/ImageController.php
+++ miro/app/Http/Controllers/ImageController.php
@@ -40,18 +40,36 @@ class ImageController extends Controller
      * @return Response
      */
     public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'file' => 'required|image'
-        ]);
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
-        $file = 'images/' . uniqid() . '.' . $request->file->extension();
-        $image = Image::create(['file' => $file]);
-        $path = $request->file->storeAs('public', $file);
-        return response()->json(['link' => $image->url]);
-    }
+   {
+      // Validation //
+      $validation = Validator::make($request->all(), [
+         'caption'     => 'required|regex:/^[A-Za-z ]+$/',
+         'description' => 'required',
+         'userfile'     => 'required|image|mimes:jpeg,png|min:1|max:250'
+      ]);
+
+      // Check if it fails //
+      if( $validation->fails() ){
+         return redirect()->back()->withInput()
+                          ->with('errors', $validation->errors() );
+      }
+
+      $image = new Image;
+
+      // upload the image //
+      $file = $request->file('userfile');
+      $destination_path = 'uploads/';
+      $filename = str_random(6).'_'.$file->getClientOriginalName();
+      $file->move($destination_path, $filename);
+      
+      // save image data into database //
+      $image->file = $destination_path . $filename;
+      $image->caption = $request->input('caption');
+      $image->description = $request->input('description');
+      $image->save();
+
+      return redirect('/')->with('message','You just uploaded an image!');
+   }
 
     /**
      * Display the specified resource.
@@ -87,9 +105,39 @@ class ImageController extends Controller
      * @return Response
      */
     public function update(Request $request, $id)
-    {
-        //
-    }
+   {
+      // Validation //
+      $validation = Validator::make($request->all(), [
+            'caption'     => 'required|regex:/^[A-Za-z ]+$/',
+            'description' => 'required',
+            'userfile'    => 'sometimes|image|mimes:jpeg,png|min:1|max:250'
+      ]);
+
+      // Check if it fails //
+      if( $validation->fails() ){
+            return redirect()->back()->withInput()
+                             ->with('errors', $validation->errors() );
+      }
+
+      // Process valid data & go to success page //
+      $image = Image::find($id);
+
+      // if user choose a file, replace the old one //
+      if( $request->hasFile('userfile') ){
+           $file = $request->file('userfile');
+           $destination_path = 'uploads/';
+           $filename = str_random(6).'_'.$file->getClientOriginalName();
+           $file->move($destination_path, $filename);
+           $image->file = $destination_path . $filename;
+      }
+        
+      // replace old data with new data from the submitted form //
+      $image->caption = $request->input('caption');
+      $image->description = $request->input('description');
+      $image->save();
+
+      return redirect('/')->with('message','You just updated an image!');
+   }
 
     /**
      * Remove the specified resource from storage.
@@ -98,11 +146,9 @@ class ImageController extends Controller
      * @return Response
      */
     public function destroy($id)
-    {
-        $image = new image();
-        $image->delete_user_images($id);
-        
-        //return Redirect::to('home');
-        return 'image just deleted';
-    }
+   {
+      $image = Image::find($id);
+      $image->delete();
+      return redirect('/')->with('message','You just uploaded an image!');
+   }
 }
