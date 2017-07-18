ea1db8a5124a708e202c1e202002d3c35c6b3031
Guinslym <guinslym@gmail.com>
2017-07-17 21:26:09 -0400

    image crud part 1

--- miro/config/app.php
+++ miro/config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -196,6 +197,8 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Illuminate\Html\FormFacade::class,
         'Html'      => Illuminate\Html\HtmlFacade::class,
+        'Formc'      =>  Collective\Html\FormFacade::class,
+        'Htmlc'      =>  Collective\Html\HtmlFacade::class,
 
     ],
 
