23f6d9db8a05820eabe910ceab5ac70df97da9ed
Guinslym <guinslym@gmail.com>
2017-07-18 23:20:51 -0400

    finish for today

---
+++ miro/resources/views/frontend/partials/contact.blade.php
@@ -0,0 +1,153 @@
+
+    <div class="container">
+      <div class="row">
+        <section class="contactForm">
+
+<style>
+  .sub-title h3 {
+    font-size: 18px;
+    line-height: 28px;
+    margin: 0 auto 70px;
+    max-width: 900px;
+    text-transform: none;
+}
+#contact input {
+    margin-bottom: 20px;
+}
+.reply input {
+    margin: 0 0 20px;
+}
+input.form-control, textarea.form-control {
+    border: solid 1px #CCC;
+    background: #fff !important;
+    box-shadow: none !important;
+}
+.form-control {
+    height: 44px;
+}
+#contact textarea {
+    height: 172px;
+    resize: none;
+}
+.reply textarea {
+    min-width: 97.5%;
+    min-height: 172px;
+    margin: 0 0 18px;
+}
+input.form-control, textarea.form-control {
+    border: solid 1px #CCC;
+    background: #fff !important;
+    box-shadow: none !important;
+}
+#contact .btn {
+    padding: 10px 40px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.btn-2:hover, .btn-2:active, .btn-2 {
+    box-shadow: 0 4px #026ea6;
+}
+.btn:focus, .btn-2, ul.why li:before, .progress .progress-bar, .accordionMod .panel-heading .current, #current:after, .flat .plan li.plan-name, .flat .plan.featured:hover li.plan-name, #filters ul li a:hover h5, #filters ul li a.active h5, .cl-effect-21 a::before, .cl-effect-21 a::after, .cl-effect-21 .active a::before, .cl-effect-21 .active a::after, .post-date a.date i, .service-icon i {
+    background: #2eaef0;
+}
+.btn-2 {
+    border-radius: 5px;
+}
+.btn-2 {
+    color: #fff;
+    transition: none;
+}
+.btn {
+    border: none;
+    font-family: inherit;
+    font-size: inherit;
+    cursor: pointer;
+    display: inline-block;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: 700;
+    outline: none;
+    position: relative;
+    z-index: 101;
+}
+.btn-2:hover, .btn-2:active, .btn-2 {
+    box-shadow: 0 4px #026ea6;
+}
+.btn-2:active {
+    color: #fff;
+    top: 6px;
+}
+input:hover, textarea:hover, input:focus, textarea:focus {
+    border-color: #C9C9C9;
+}
+div.contact-form{
+  margin:60px 0;
+}
+</style>
+<div class="contact-form">
+                        <div class="container">
+                              <!-- Title row -->
+                              <div class="row">
+                                    <div class="col-md-12 big-title wow bounceIn animated" style="visibility: visible;">
+                                          <h2>Contact</h2>
+                                    </div>
+
+                                    <div class="clearfix"></div>
+
+                                    <div class="col-md-12 sub-title text-center wow slideInRight animated" style="visibility: visible;">
+                                          <h3>Contact with us to learn more about all of our Web Design Services!</h3>
+                                    </div>
+                              </div>
+                              <!-- /Title row -->
+
+                              <!-- Form element row -->
+                              <div class="row text-center">
+                                    <div class="col-md-2"></div>
+
+                                    <div class="col-md-8">
+                                          <form method="post" class="reply" id="contact">
+                                                <fieldset>
+                                                      <div class="row">
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wow fadeInLeft animated" style="visibility: visible;">
+                                                                  <div class="row">
+                                                                        <div class="col-md-12">
+                                                                              <input class="form-control" id="name" name="name" type="text" placeholder="Name" value="" required="">
+                                                                        </div>
+                                                                        <div class="col-md-12">
+                                                                              <input class="form-control" type="email" id="email" name="email" placeholder="Email" value="" required="">
+                                                                        </div>
+                                                                        <div class="col-md-12">
+                                                                              <input class="form-control" type="text" id="subject" name="subject" placeholder="Subject" value="" required="">
+                                                                        </div>
+                                                                  </div>
+                                                            </div>
+
+                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wow fadeInRight animated" style="visibility: visible;">
+                                                                  <div class="row">
+                                                                        <div class="col-md-12">
+                                                                              <textarea class="form-control" id="text" name="text" rows="3" cols="40" placeholder="Your Message" required=""></textarea>
+                                                                        </div>
+                                                                  </div>
+                                                            </div>
+                                                      </div>
+                                                      <div class="row">
+                                                            <div class="col-md-12 wow fadeInUp animated" style="visibility: visible;">
+                                                                  <button type="submit" class="btn btn-2 btn-2c">Send</button>
+                                                                  <div class="success alert-success alert" style="display:none">Your message has been sent successfully.</div>
+                                                                  <div class="error alert-danger alert" style="display:none">E-mail must be valid and message must be longer than 100 characters.</div>
+                                                            </div>
+                                                      </div>
+                                                </fieldset>
+                                          </form>
+                                    </div>
+
+                                    <div class="col-md-2"></div>
+                              </div>
+                              <!-- /Form element row -->
+                        </div>
+                  </div>
+
+        </section>
+      </div>
+    </div>
--- miro/resources/views/frontend/partials/socials.blade.php
+++ miro/resources/views/frontend/partials/socials.blade.php
@@ -46,34 +46,34 @@
                     <ul class="list-inline social-buttons">
                         
                         
-                        <li class="social-button-item"><a href="mailto:kdhuff@illinois.edu"><i class="fa fa-envelope-o"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-envelope-o"></i></a>
                         </li>
                         
-                        <li class="social-button-item"><a href="https://github.com/arfc"><i class="fa fa-slack" alt="slack"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-slack" alt="slack"></i></a>
                         </li>
                         
                         <li class="social-button-item"><a href=""><i class="fa fa-linkedin"></i></a>
                         </li>
                         
-                        <li class="social-button-item"><a href="https://twitter.com/katyhuff"><i class="fa fa-twitter"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-twitter"></i></a>
                         </li>
                         
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-snapchat"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-snapchat"></i></a>
                         </li>
 
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-whatsapp"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-whatsapp"></i></a>
                         </li>
 
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-youtube"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-youtube"></i></a>
                         </li>
 
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-instagram"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-instagram"></i></a>
                         </li>   
 
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-facebook-official"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-facebook-official"></i></a>
                         </li>   
 
-                        <li class="social-button-item"><a href="http://www.zotero.org/groups/arfc"><i class="fa fa-tumblr-square"></i></a>
+                        <li class="social-button-item"><a href=""><i class="fa fa-tumblr-square"></i></a>
                         </li>                        
                     </ul>
                 </div>
