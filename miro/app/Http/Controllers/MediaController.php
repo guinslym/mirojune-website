83c1653ae6edab3a0a4b658d5c9404583a1616bf
Guinslym <guinslym@gmail.com>
2017-07-22 04:29:35 -0400

    end of the day

--- miro/app/Http/Controllers/MediaController.php
+++ miro/app/Http/Controllers/MediaController.php
@@ -12,6 +12,10 @@ use Validator;
 
 class MediaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
--- miro/app/Http/routes.php
+++ miro/app/Http/routes.php
@@ -22,16 +22,43 @@ Route::get('/paintings', ['as' => 'fontend-painting', 'uses' => 'AboutController
 Route::get('/medias', ['as' => 'fontend-admin-medias', 'uses' => 'AboutController@painting']);
 
 
+// Only authenticated users may enter...
+Route::group(['middleware' => 'auth'], function()
+{
+    //Route::resource('todo', 'TodoController', ['only' => ['index']]);
+	Route::resource('/image', 'ImageController');
+	Route::resource('/medias', 'MediaController');
+	Route::resource('/profile', 'MediaController');
+});
+// Using A Controller...
 
-
-//Route::get('/', function(){ return redirect('/image'); });
-Route::resource('/image', 'ImageController');
-Route::resource('/medias', 'MediaController');
-Route::resource('/profile', 'MediaController');
-
+Route::get('profilelkjlkj', [
+    'middleware' => 'auth',
+    'uses' => 'ProfileController@show'
+]);
 
 
 Route::get('contact', 
   ['as' => 'contact', 'uses' => 'AboutController@create']);
 Route::post('contact', 
   ['as' => 'contact_store', 'uses' => 'AboutController@store']);
+
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('home', function(){
+        if (Auth::guest()) {
+            // $request->user() returns an instance of the authenticated user...
+            return Redirect::to('auth/login');
+        }else{
+			echo 'Welcome home' . Auth::user()->email . "."; 
+        }
+});
