d88f9ae84b4e3247aecf3a5870e38351aff378cd
Guinslym <guinslym@gmail.com>
2017-08-06 10:43:16 -0400

    block of wall aint work

---
+++ miro/resources/views/frontend/partials/picture-block-of-walls.blade.php
@@ -0,0 +1,126 @@
+
+<style>
+  #portfolio-wrap {
+    position: relative;
+    padding: 0;
+    width: 100%;
+    margin: 0 auto;
+    display: block;
+}
+</style>
+
+<div id="portfolio-wrap" style="position: relative; overflow: hidden; height: 490px;" class="isotope">
+                    
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four wordpress html css isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/JQEw7d_miro3.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  -->       
+                    
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four php jquery isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(327px, 0px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/M6EgtI_miro4.jpg" alt="">                                   
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  -->      
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four wordpress css html isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(654px, 0px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/JQEw7d_miro3.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/JQEw7d_miro3.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four branding jquery isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(981px, 0px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/M6EgtI_miro4.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/M6EgtI_miro4.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                        
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four php html css isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 245px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/M6EgtI_miro4.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/M6EgtI_miro4.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four wordpress jquery isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(327px, 245px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/JQEw7d_miro3.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/JQEw7d_miro3.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four css php html jquery isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(654px, 245px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/M6EgtI_miro4.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/M6EgtI_miro4.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                        
+                        <!-- Portfolio Item With PrettyPhoto  -->                 
+                        <div class="portfolio-item one-four wordpress php css isotope-item" style="position: absolute; left: 0px; top: 0px; transform: translate3d(981px, 245px, 0px); width: 327px;">
+                              <div class="img-holder">
+                                    <img class="img-responsive" src="http://localhost:8000/uploads/JQEw7d_miro3.jpg" alt="">
+                                    <div class="img-overlay"></div>
+                                    <div class="overlay-effect">
+                                        <span class="RoundBg"><a data-gal="prettyPhoto[galname]" href="http://localhost:8000/uploads/JQEw7d_miro3.jpg"><i class="fa fa-search"></i></a>
+                                        </span>
+                                        <span class="RoundBg"><a href=""><i class="fa fa-link"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- /Portfolio Item With PrettyPhoto  --> 
+                  
+                              
+                        
+                              
+                             
+                   </div>
